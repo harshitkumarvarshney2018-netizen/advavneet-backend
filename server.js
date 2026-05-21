0df467a3263bae68cfba4ed73491c1b74f71a273
Harshit Varshney <harshitkumarvarshney2018@gmail.com>
2026-05-21 21:03:00 +0530

    gmail fix

--- server.js
+++ server.js
@@ -13,15 +13,9 @@ const nodemailer = require("nodemailer");
 // Set up the email sender
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false   // ← THIS was killing it silently
   }
 });
 
